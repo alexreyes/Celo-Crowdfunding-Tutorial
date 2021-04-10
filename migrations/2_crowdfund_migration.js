1fec91b5e8614a8731d556a96dfbe15892748b3b
Alex Reyes <reyesruizalex@gmail.com>
2021-04-10 00:03:08 -0400

    remove change

--- migrations/2_crowdfund_migration.js
+++ migrations/2_crowdfund_migration.js
@@ -1,6 +1,5 @@
 const CeloCrowdfund = artifacts.require("CeloCrowdfund");
 
 module.exports = function (deployer) {
-  // 0x874069fa1eb16d44d622f2e0ca25eea172369bc1 == cUSD contract
-  deployer.deploy(CeloCrowdfund, '0x874069fa1eb16d44d622f2e0ca25eea172369bc1');
+  deployer.deploy(CeloCrowdfund);
 };
